afda30f1f847c81060dbe7308f6d0206a10bc70c
brosephstalin3fm <jacksonsplinter@gmail.com>
2022-09-11 17:14:07 -0400

    Problem  1 formatted

--- Answers/src/answers/Problem_1.java
+++ Answers/src/answers/Problem_1.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package answers;
+/*
+@author Jackson Splinter
+Given an array of integers nums and an integer target, return indices of the two numbers 
+such that they add up to target.
 
-/**
- *
- * @author jackson
- */
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
 public class Problem_1 
 {
     public int[] twoSum(int[] nums, int target) 
