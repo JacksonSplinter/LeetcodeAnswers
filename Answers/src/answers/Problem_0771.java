00c5f82f035caa09d78ae1aab0f207c65cbd7a2a
brosephstalin3fm <jacksonsplinter@gmail.com>
2022-09-24 19:06:41 -0400

    Create Problem_0771.java
    Problem 771 finished

---
+++ Answers/src/answers/Problem_0771.java
@@ -0,0 +1,39 @@
+package answers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ @author Jackson Splinter
+
+ You're given strings jewels representing the types of stones that are jewels,
+ and stones representing the stones you have. Each character in stones is a type of stone you have.
+You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ */
+public class Problem_0771 {
+    public static int numJewelsInStones(String jewels, String stones) 
+    {
+        Queue<Character> queue = new LinkedList<>();
+        int ans = 0;
+        for (char c : jewels.toCharArray()) 
+        {
+            if (!queue.contains(c)) 
+            {
+                queue.offer(c);
+            }
+        }
+         for (char c : stones.toCharArray()) 
+        {
+            if (queue.contains(c)) 
+            {
+                ans++;
+            }
+        }
+        
+        return ans;
+        
+    }
+    
+}
